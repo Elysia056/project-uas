345f9429d5d7b3d7bc397fcd9dcb3b478e5fda09
Richard-H-Y <vxrqlo42@gmail.com>
2026-05-28 10:55:17 +0700

    2nd phase

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-// DIKERJAKAN OLEH: R
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $fillable = ['name', 'description'];
 
-    // Relasi: Category punya banyak Product
     public function products()
     {
         return $this->hasMany(Product::class);
